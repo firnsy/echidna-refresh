61edaa3ebc11ab293b31e8e09fb07aac53f8223b
firnsy <firnsy@securixlive.com>
2012-11-06 21:32:44 +1100

    updated: session and event model realignment to spec.

--- lib/Echidna/Model/Event.pm
+++ lib/Echidna/Model/Event.pm
@@ -6,23 +6,39 @@ use 5.010;
 use base qw(Echidna::Model::Object);
 
 __PACKAGE__->set_properties({
-    id             => ['int'],
-    node_id        => ['text'],
-    net_src_ip     => ['ip'],
-    net_src_port   => ['int'],
-    net_dst_ip     => ['ip'],
-    net_dst_port   => ['int'],
-    net_protocol   => ['int'],
+    # required
 
-    timestamp      => 'timestamp',
-    classification => 'text',
-    net_version    => 'int',
-    sig_id         => 'int',
-    sig_revision   => 'int', 
-    sig_priority   => 'int',
-    sig_message    => 'text',
-    sig_category   => 'text',
-    meta           => 'text',
+    # sha256(concat(timestamp,net_src_ip,net_src_port,net_dst_ip,net_dst_port,net_protocol))
+    id                => ['sha256'],
+    node_id           => ['sha256'],
+
+    # sha256(concat(net_src_ip,net_src_port,net_dst_ip,net_dst_port,net_protocol,sig_type,sig_id,sig_revision))
+    evt_corr_id       => ['sha256'],
+
+    # sha256(concat(net_src_ip,net_src_port,net_dst_ip,net_dst_port,net_protocol))
+    ssn_corr_id       => ['sha256'],
+
+    net_version       => ['int'],
+    net_src_ip        => ['ip'],
+    net_src_port      => ['int'],
+    net_dst_ip        => ['ip'],
+    net_dst_port      => ['int'],
+    net_protocol      => ['int'],
+
+    timestamp         => ['timestamp'],
+    sig_type          => ['int'],
+    sig_id            => ['int'],
+    sig_revision      => ['int'], 
+    sig_category      => ['text'],
+    sig_priority      => ['int'],
+    classification    => ['text'],
+
+    # optional
+    sig_message       => 'text',
+
+    # optional - application specific
+    meta              => 'text',
+    meta_by2_event_id => 'any',
 });
 
 1;
--- lib/Echidna/Model/Session.pm
+++ lib/Echidna/Model/Session.pm
@@ -7,8 +7,14 @@ use base qw(Echidna::Model::Object);
 
 __PACKAGE__->set_properties({
     # required
-    id                    => ['int'],
+
+    # sha256(concat(time_start,time_end,net_src_ip,net_src_port,net_dst_ip,net_dst_port,net_protocol))
+    id                    => ['sha256'],
     node_id               => ['sha256'],
+
+    # sha256(concat(net_src_ip,net_src_port,net_dst_ip,net_dst_port,net_protocol))
+    ssn_corr_id           => ['sha256'],
+
     time_start            => ['datetime'],
     time_end              => ['datetime'],
     net_src_ip            => ['ip'],
@@ -22,21 +28,19 @@ __PACKAGE__->set_properties({
     net_dst_total_packets => ['int'],
 
     # optional
-    timestamp          => 'timestamp',
-    time_duration      => 'text',
-    net_version        => 'int',
-    net_src_flags      => 'int',
-    net_dst_flags      => 'int',
-    file_name_start    => 'text',
-    file_offset_start  => 'any',
-    file_name_end      => 'text',
-    file_offset_end    => 'any',
-    meta          => 'any',
+    timestamp             => 'timestamp',
+    time_duration         => 'text',
+    net_version           => 'int',
+    net_src_flags         => 'int',
+    net_dst_flags         => 'int',
+    file_name_start       => 'text',
+    file_offset_start     => 'int',
+    file_name_end         => 'text',
+    file_offset_end       => 'int',
+
+    # optional - application specific
+    meta                  => 'any',
+    meta_cxt_id           => 'any',
 });
-
-sub process {
-    my ($self) = @_;
-
-}
 
 1;

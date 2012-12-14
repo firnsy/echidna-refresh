b5fbe60cd527ee5474bbbfb3f797e1a70f5de31b
firnsy <firnsy@securixlive.com>
2012-12-14 13:42:49 +1100

    updated: fleshed out intial events and sessions pages.

--- public/js/echidna.passivedns.js
+++ public/js/echidna.passivedns.js
@@ -1,4 +1,64 @@
 function PassiveDNSCtrl( $scope ) {
+  $scope.dns = [{"timestamp":1353554322,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"realtime.services.disqus.com.","query_type":"A","query_answer":"67.228.181.220","ttl":141},
+{"timestamp":1353554322,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"realtime.services.disqus.com.","query_type":"A","query_answer":"67.228.181.218","ttl":141},
+{"timestamp":1353554322,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"realtime.services.disqus.com.","query_type":"A","query_answer":"67.228.181.219","ttl":141},
+{"timestamp":1353554342,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"tvrage.com.","query_type":"A","query_answer":"80.246.178.98","ttl":5190},
+{"timestamp":1353554342,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.amazon.co.uk.","query_type":"A","query_answer":"178.236.7.220","ttl":4},
+{"timestamp":1353554402,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"allmusic.com.","query_type":"A","query_answer":"144.198.225.72","ttl":120},
+{"timestamp":1353554402,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.imdb.com.","query_type":"CNAME","query_answer":"us.dd.imdb.com.","ttl":2755},
+{"timestamp":1353554402,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.imdb.com.","query_type":"A","query_answer":"207.171.162.180","ttl":12},
+{"timestamp":1353554402,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.newzbin2.es.","query_type":"A","query_answer":"85.112.165.88","ttl":35921},
+{"timestamp":1353554403,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.tvrage.com.","query_type":"A","query_answer":"80.246.178.98","ttl":6865},
+{"timestamp":1353554492,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.imdb.com.","query_type":"A","query_answer":"72.21.215.52","ttl":6},
+{"timestamp":1353554501,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"github.com.","query_type":"A","query_answer":"207.97.227.239","ttl":100},
+{"timestamp":1353554503,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"a248.e.akamai.net.","query_type":"A","query_answer":"61.9.129.151","ttl":9},
+{"timestamp":1353554503,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"a248.e.akamai.net.","query_type":"A","query_answer":"61.9.129.145","ttl":9},
+{"timestamp":1353554503,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"ssl.google-analytics.com.","query_type":"CNAME","query_answer":"ssl-google-analytics.l.google.com.","ttl":1966},
+{"timestamp":1353554503,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"ssl.google-analytics.com.","query_type":"A","query_answer":"74.125.237.158","ttl":113},
+{"timestamp":1353554503,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"secure.gravatar.com.","query_type":"CNAME","query_answer":"lb.gravatar.com.","ttl":3600},
+{"timestamp":1353554503,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"secure.gravatar.com.","query_type":"A","query_answer":"72.233.61.125","ttl":75},
+{"timestamp":1353554503,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"secure.gravatar.com.","query_type":"A","query_answer":"72.233.69.4","ttl":75},
+{"timestamp":1353554503,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"secure.gravatar.com.","query_type":"A","query_answer":"72.233.69.5","ttl":75},
+{"timestamp":1353554503,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"secure.gravatar.com.","query_type":"A","query_answer":"72.233.61.123","ttl":75},
+{"timestamp":1353554504,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"secure.gaug.es.","query_type":"A","query_answer":"64.22.108.249","ttl":1359},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"CNAME","query_answer":"clients.l.google.com.","ttl":214},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"A","query_answer":"74.125.237.130","ttl":299},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"A","query_answer":"74.125.237.137","ttl":299},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"A","query_answer":"74.125.237.136","ttl":299},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"A","query_answer":"74.125.237.134","ttl":299},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"A","query_answer":"74.125.237.142","ttl":299},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"A","query_answer":"74.125.237.131","ttl":299},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"A","query_answer":"74.125.237.135","ttl":299},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"A","query_answer":"74.125.237.133","ttl":299},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"A","query_answer":"74.125.237.128","ttl":299},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"A","query_answer":"74.125.237.129","ttl":299},
+{"timestamp":1353554514,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"clients4.google.com.","query_type":"A","query_answer":"74.125.237.132","ttl":299},
+{"timestamp":1353554522,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.amazon.co.uk.","query_type":"A","query_answer":"178.236.6.251","ttl":6},
+{"timestamp":1353554612,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.amazon.co.uk.","query_type":"A","query_answer":"176.32.108.186","ttl":50},
+{"timestamp":1353554612,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.imdb.com.","query_type":"A","query_answer":"72.21.203.211","ttl":60},
+{"timestamp":1353554432,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.amazon.co.uk.","query_type":"A","query_answer":"178.236.7.220","ttl":37},
+{"timestamp":1353554552,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.imdb.com.","query_type":"A","query_answer":"72.21.215.52","ttl":25},
+{"timestamp":1353554612,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.imdb.com.","query_type":"CNAME","query_answer":"us.dd.imdb.com.","ttl":3099},
+{"timestamp":1353554612,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"tvrage.com.","query_type":"A","query_answer":"80.246.178.98","ttl":5190},
+{"timestamp":1353554684,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"realtime.services.disqus.com.","query_type":"A","query_answer":"67.228.181.219","ttl":478},
+{"timestamp":1353554684,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"realtime.services.disqus.com.","query_type":"A","query_answer":"67.228.181.218","ttl":478},
+{"timestamp":1353554684,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"realtime.services.disqus.com.","query_type":"A","query_answer":"67.228.181.220","ttl":478},
+{"timestamp":1353554612,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.tvrage.com.","query_type":"A","query_answer":"80.246.178.98","ttl":6993},
+{"timestamp":1353554612,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"www.newzbin2.es.","query_type":"A","query_answer":"85.112.165.88","ttl":35921},
+{"timestamp":1353554672,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"allmusic.com.","query_type":"A","query_answer":"144.198.225.72","ttl":300},
+{"timestamp":1354408877,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"realtime.services.disqus.com.","query_type":"A","query_answer":"67.228.181.219","ttl":382},
+{"timestamp":1354408877,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"realtime.services.disqus.com.","query_type":"A","query_answer":"67.228.181.218","ttl":382},
+{"timestamp":1354408877,"net_src_ip":"192.168.10.20","net_dst_ip":"192.168.10.1","rr_class":"IN","query":"realtime.services.disqus.com.","query_type":"A","query_answer":"67.228.181.220","ttl":382}];
+
+  $scope.dnsCount = function() {
+    return $scope.dns.length;
+  };
+
+
+  $scope.formatTimestamp = function(t) {
+    return t;
+  };
+
   console.log('PassiveDNSCtrl in scope.');
 };
 
